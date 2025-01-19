75e9f0855e75eb82ae5be2d31332a134776ef691
Alessandra <alessanas@hotmail.com>
2025-01-19 16:45:48 -0300

    Salvando arquivo Primeira Classe

---
+++ src/br/com/ale/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+package br.com.ale;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		String nome = args[0];
+		System.out.println("Olá "+args[0] + " " + args[1]);
+	}
+}
